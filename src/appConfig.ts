16cfa92840bc77ff91acc3368927ec67b8393e46
weixuefeng <weixuefeng@diynova.com>
2022-10-21 14:58:52 +0800

    feat: update app config for head

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,12 +1,12 @@
 export const app: { [key: string]: string } = {
-  name: 'Nextjs Starter', // web app name
-  title: 'Nextjs Starter Boilerplate', // website title
-  description: 'Nextjs Starter Boilerplate by @arisac', // description of website
-  keywords: 'nextjs, react, tailwindcss, boilerplate', // keywords for SEO
-  url: 'https://nextjs-starter.aris.ac', // website url
+  name: 'Wave App', // web app name
+  title: 'Wave', // website title
+  description: 'THE WEB3 PLATFORMOF GLOBAL DIGITALENTERTAINMENT', // description of website
+  keywords: 'wave, nft, evt, web3, newchain', // keywords for SEO
+  url: 'https://web.waveuniverse.org', // website url
   image: '/icon-512x512.png', // image url for SEO
   favicon: '/favicon.ico', // favicon url
-  twitter: '@arisdotac', // twitter account
+  twitter: '@wave_universe', // twitter account
   themeColor: '#FFFFFF', // theme color
   themeColorDark: '#000000', // theme color dark
 }
