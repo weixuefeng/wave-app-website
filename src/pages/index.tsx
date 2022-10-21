3d9dcd3add8db7abc2d2eff4ed8c3c00e2c0c96b
weixuefeng <weixuefeng@diynova.com>
2022-10-21 21:19:19 +0800

    feat: remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,14 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-12 19:08:34
  * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-10-21 19:45:52
+ * @LastEditTime: 2022-10-21 21:18:07
  * @FilePath: /wave-app-webiste/src/pages/index.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE)
  */
 
 import React, { useState, useEffect } from 'react'
 import NormalLayout from 'components/layout/normalLayout'
 import { PageModel } from 'model/navModel'
 import { useTranslation } from 'react-i18next'
-import { postRequest } from 'services/getAxios'
 import { sign } from 'utils/sign_utils'
 export default Home
 
@@ -20,7 +18,8 @@ function Home() {
   return <>{NormalLayout(Main(), pageModel)}</>
 }
 
-const collectionUrl = '/api/hello'
+const newLocal = '/api/hello'
+const collectionUrl = newLocal
 function Main() {
   let { i18n } = useTranslation()
   const { t } = useTranslation()
