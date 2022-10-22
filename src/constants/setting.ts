acffb79ff383ff4f3d6b055da5034b719e407557
weixuefeng <weixuefeng@diynova.com>
2022-10-22 18:08:50 +0800

    feat: add is debug config for sentry

--- src/constants/setting.ts
+++ src/constants/setting.ts
@@ -2,10 +2,11 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-13 11:06:46
  * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-10-21 17:06:34
+ * @LastEditTime: 2022-10-22 18:06:33
  * @FilePath: /wave-app-webiste/src/constants/setting.ts
  */
 export const WAVE_BASE_URL = process.env.NEXT_PUBLIC_WAVE_BASE_URL
 export const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION
 export const APP_KEY = process.env.NEXT_PUBLIC_APP_KEY
 export const APP_SECRET = process.env.NEXT_PUBLIC_APP_SECRET
+export const IS_DEBUG = process.env.NEXT_PUBLIC_IS_DEBUG == "true"
