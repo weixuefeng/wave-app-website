e927be25f387b075a1d0af80c360778c18feba34
zxt0805 <zhuxiaotong@diynova.com>
2022-10-27 19:06:26 +0800

    test: commit

--- src/components/home/BannerComponent.tsx
+++ src/components/home/BannerComponent.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: zhuxiaotong zhuxiaotong@diynova.com
  * @Date: 2022-10-24 11:54:01
- * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
- * @LastEditTime: 2022-10-24 15:18:50
+ * @LastEditors: zxt0805 zhuxiaotong@diynova.com
+ * @LastEditTime: 2022-10-27 19:04:56
  * @FilePath: /wave-app-website/src/components/home/BannerComponent.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -37,7 +37,6 @@ export default function BannerComponent(props) {
         {banners.map((item, index) => {
           return <SwiperSlide key={index}><img alt="" src={item.image} /></SwiperSlide>
         })}
-        
       </Swiper>
     )
   } else {
