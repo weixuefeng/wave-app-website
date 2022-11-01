4e6eb44210a96555231920351645ee966e6f8c19
weixuefeng <weixuefeng@diynova.com>
2022-11-01 15:18:38 +0800

    refactor: home style

--- src/components/home/BlindBoxComponent.tsx
+++ src/components/home/BlindBoxComponent.tsx
@@ -1,17 +1,101 @@
 import { useTranslation } from 'react-i18next'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export function BlindBox(props) {
   const { item } = props
   const { t } = useTranslation()
+
+  const [remainSecond, setRemainSecond] = useState(0)
+  let timer
+
+  useEffect(() => {
+    if (item) {
+      const remainTime = parseInt(item.sell_start_time) - parseInt(item.system_time)
+      setRemainSecond(remainTime)
+      if (item.sell_status == 0 && remainTime <= 86400) {
+        if (timer) {
+          clearInterval(timer)
+        }
+        countDown(remainTime)
+      }
+    }
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  function countDown(startTime) {
+    let countDownTime = startTime
+    timer = setInterval(() => {
+      if (countDownTime > 1) {
+        setRemainSecond(--countDownTime)
+      } else {
+        clearInterval(timer)
+      }
+    }, 1000)
+  }
+
+  function calculateCountdown(remain) {
+    let hrs = Math.floor(remain / 3600) || 0
+    remain = remain % 3600
+    let min = Math.floor(remain / 60) || 0,
+      sec = remain % 60
+    return `${fillZero(hrs)}:${fillZero(min)}:${fillZero(sec)}`
+  }
+
+  function statusJudge() {
+    if (item.sell_status == 0) {
+      if (remainSecond > 86400) {
+        return <div className="status-onimg">{t('UPCOMMING_DROP')}</div>
+      } else {
+        return <div className="status-onimg">{t('COMING_SOON')}</div>
+      }
+    } else if (item.sell_status == 1) {
+      return <div className="status-onimg">{t('LIVE_DROP')}</div>
+    } else {
+      return <div className="status-onimg disabled">{t('SOLD_OUT')}</div>
+    }
+  }
+
+  function timeJudge() {
+    if (item.sell_status == 0) {
+      if (remainSecond > 86400) {
+        return <div className="time-onimg">{t('STARTSAT') + ' ' + getTimeStr(item.sell_start_time)}</div>
+      } else if (remainSecond > 0) {
+        return <div className="time-onimg">{t('STARTSIN') + ' ' + calculateCountdown(remainSecond)}</div>
+      } else {
+        return <></>
+      }
+    } else {
+      if (item.reveals_time > 0) {
+        return <div className="time-onimg">{t('REVEALSAT') + ' ' + getTimeStr(item.reveals_time)}</div>
+      } else {
+        return <></>
+      }
+    }
+  }
+
+  function getTimeStr(timestamp) {
+    let time = new Date(timestamp * 1000),
+      timeZone = (time.getTimezoneOffset() * -1) / 60
+    return `${fillZero(time.getMonth() + 1)}.${fillZero(time.getDate())} ${fillZero(time.getHours())}:${fillZero(
+      time.getMinutes()
+    )}:${fillZero(time.getSeconds())}(UTC${timeZone > 0 ? '+' + timeZone : timeZone})`
+  }
+
+  function fillZero(num) {
+    return Number(num) > 9 ? num.toString() : '0' + num.toString()
+  }
 
   return (
     <div className="item">
       <div className="cover">
         <div className="perfect_square">
           <img alt={item.name} src={item.image} />
-          <div className="status">sold out</div>
+          {/* <div className="status">sold out</div> */}
+          {timeJudge()}
+          {statusJudge()}
         </div>
       </div>
       <div className="profile">
--- src/components/home/HomeDataComonent.tsx
+++ src/components/home/HomeDataComonent.tsx
@@ -2,11 +2,10 @@
  * @Author: zxt0805 zhuxiaotong@diynova.com
  * @Date: 2022-10-24 11:54:01
  * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-11-01 14:44:52
+ * @LastEditTime: 2022-11-01 15:17:54
  * @FilePath: /wave-app-webiste/src/components/home/HomeDataComonent.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import MysteryItem from 'components/home/mystery/mystery_item'
 import RecommendItem from 'components/home/recommend/recommend_item'
 import React from 'react'
 import BlindBoxComponent from './BlindBoxComponent'
@@ -22,20 +21,6 @@ export default function HomeDataComonent(props) {
   return (
     <div id="home" className="container">
       <BlindBoxComponent blindBox={blindBox} />
-      <div className="blind">
-        <p className="title">MyStery Box</p>
-        <div className="flex flex-wrap justify-between">
-          {blindBox &&
-            blindBox.map((item, index) => {
-              return (
-                <div className="index-mystery-wrap" key={index}>
-                  <MysteryItem collectionInfo={item} key={index} width="w-[343px]" height="h-[343px]" />
-                </div>
-              )
-            })}
-        </div>
-      </div>
-
       <div className="recommend">
         <p className="title">Recommend</p>
         <div className="flex flex-wrap justify-between">
