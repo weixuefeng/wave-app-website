7112ac1d116d945ea69fe5557bf1281f59fec95e
weixuefeng <weixuefeng@diynova.com>
2022-11-02 20:14:18 +0800

    fix: collection page style

--- src/components/home/RecommendComponent.tsx
+++ src/components/home/RecommendComponent.tsx
@@ -99,42 +99,13 @@ export function CollectionAsset(props) {
       <div className="cover">
         <div className="perfect_square">
           <img alt={item.name} src={item.image} />
-          {(item.type === AssetType.MOVIE || item.type === AssetType.SERIOS) && <div className="evt-icon">EVT</div>}
-
-          {item.type === AssetType.COLLECTION && (
             <div className="collection">
               <img alt={item.name} src={item.icon} className="img-icon" />
             </div>
-          )}
         </div>
       </div>
-
       <div className="info-box">
-        {item.type !== AssetType.COLLECTION ? (
-          <div className="title">
-            <p className="name">{item.name}</p>
-            {item.running_time !== 0 && <p className="time">{item.running_time}</p>}
-          </div>
-        ) : (
-          <div className="title title-collection">{item.name}</div>
-        )}
-        {(item.lowest_sell_price || item.highest_bid_price) && (
-          <div className="price-info">
-            {item.lowest_sell_price && (
-              <>
-                <div className="price-title">{t('FLOOR_PRICE')}</div>
-                <div className="price-num">{Number(item.lowest_sell_price)} NEW</div>
-              </>
-            )}
-            {!item.lowest_sell_price && item.highest_bid_price && (
-              <>
-                <div className="price-title">{t('FLOOR_PRICE')}</div>
-                <div className="price-num">{Number(item.highest_bid_price)} NEW</div>
-              </>
-            )}
-          </div>
-        )}
-        {item.type === AssetType.MOVIE && <img src="/assets/image/play_icon.png" alt="" />}
+        <div className="title-collection">{item.name}</div>
       </div>
     </div>
   )
