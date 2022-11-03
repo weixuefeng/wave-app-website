df67e066c161cbb192806cacae42f3a703f2a3db
weixuefeng <weixuefeng@diynova.com>
2022-11-03 20:29:18 +0800

    fix: init user info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@
  * @Author: zhuxiaotong zhuxiaotong@diynova.com
  * @Date: 2022-10-24 11:54:01
  * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-11-03 19:14:19
+ * @LastEditTime: 2022-11-03 20:27:58
  * @FilePath: /wave-app-webiste/src/pages/_app.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,7 +13,30 @@ import 'i18n'
 import { useRouter } from 'next/router'
 import HeadGlobal from 'components/HeadGlobal'
 import { Provider } from 'react-redux'
-import store from '../store/store'
+import store, { useAppDispatch } from '../store/store'
+import { useEffect } from 'react'
+import { getLocalData } from 'localstorage/localstorage'
+import { LocalKey } from 'constants/key'
+import { UserInfo } from 'model/user'
+import { updateUserInfo } from 'reducer/userReducer'
+
+function InitComponent() {
+  const dispatch = useAppDispatch()
+
+  function initUser() {
+    const userInfo = getLocalData(LocalKey.USER, '')
+    let user
+    if (userInfo && userInfo) {
+      user = JSON.parse(userInfo) as UserInfo
+      dispatch(updateUserInfo(user))
+    }
+  }
+
+  useEffect(() => {
+    initUser()
+  }, [])
+  return (<></>)
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -21,6 +44,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider defaultTheme="system" attribute="class">
       <HeadGlobal />
       <Provider store={store}>
+        <InitComponent/>
         <Component key={router.asPath} {...pageProps} />
       </Provider>
     </ThemeProvider>
--- src/reducer/userReducer.ts
+++ src/reducer/userReducer.ts
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-11-01 15:43:16
  * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-11-03 19:15:03
+ * @LastEditTime: 2022-11-03 20:28:26
  * @FilePath: /wave-app-webiste/src/reducer/userReducer.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,14 +17,9 @@ interface UserState {
   status: string
 }
 
-const userInfo = getLocalData(LocalKey.USER, '')
-let user
-if (userInfo && userInfo) {
-  user = JSON.parse(userInfo) as UserInfo
-}
 
 const initialState = {
-  currentUser: user,
+  currentUser: undefined,
   status: 'idle',
 } as UserState
 
