d0b5f29c04bd4d47a6f8846bf06476ddc1e5afd0
weixuefeng <weixuefeng@diynova.com>
2022-11-03 13:53:29 +0800

    feat: update style

--- src/components/asset/ChainInfoComponent.tsx
+++ src/components/asset/ChainInfoComponent.tsx
@@ -6,19 +6,19 @@ export default function ChainInfoComponent(props) {
   const {address, tokenStandard, blockChain, creatorEariningPercent} = props
   return (
     <div className="chain-content">
-      <div className="item">
+      <div className="chain-item ">
         <p className="label">Contract address</p>
         <p className="value">{splitAddress(address)}<ClipboardDocumentIcon onClick={() => copyContent(address)}/></p>
       </div>
-      <div className="item">
+      <div className="chain-item ">
         <p className="label">Token Standard</p>
         <p className="value">{tokenStandard}</p>
       </div>
-      <div className="item">
+      <div className="chain-item ">
         <p className="label">Blockchain</p>
         <p className="value">{blockChain}</p>
       </div>
-      <div className="item">
+      <div className="chain-item">
         <p className="label">Creator Earnings</p>
         <p className="value">{creatorEariningPercent}<InformationCircleIcon/></p>
       </div>
