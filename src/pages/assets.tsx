1743b0c986fd7580d79bd75bf9d9539fb7092f59
weixuefeng <weixuefeng@diynova.com>
2022-11-04 20:48:50 +0800

    feat: update deposit

--- src/pages/assets.tsx
+++ src/pages/assets.tsx
@@ -1,5 +1,52 @@
-import React from 'react'
+import useWallet from 'hooks/userWallet'
+import { OfferType } from 'model/offer'
+import { UserInfo } from 'model/user'
+import React, { useEffect } from 'react'
+import { selectUser } from 'reducer/userReducer'
+import Http from 'services/http'
+import { useAppSelector } from 'store/store'
 
-export default function assets() {
+export default function Assets() {
+  const currentUser = useAppSelector(selectUser) as UserInfo
+
+  useEffect(() => {
+    if (currentUser) {
+      getMyAssetList()
+    }
+  }, [currentUser])
+
+  function getOrderOnSale() {
+    Http.getInstance()
+      .getOrderOnSale(currentUser.id, 1)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  function getOrderOffer() {
+    Http.getInstance()
+      .getOrderOffer(currentUser.id, 1, OfferType.MADE)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  function getMyAssetList() {
+    Http.getInstance()
+      .getMyAssetList(currentUser.id, 1)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   return <div>assets</div>
 }
