21de40cbbf8bb8399f6b49380d7a9fe956224cf8
liukeke <liukeke@diynova.com>
2022-11-04 14:50:08 +0800

    feat: loading

---
+++ src/components/loading/loading.tsx
@@ -0,0 +1,31 @@
+/*
+ * @Author: liukeke liukeke@diynova.com
+ * @Date: 2022-10-13 11:26:10
+ * @LastEditors: liukeke liukeke@diynova.com
+ * @LastEditTime: 2022-11-04 14:47:54
+ * @FilePath: /wave-app-webiste/src/components/footer/footer.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+import React from 'react'
+import { Skeleton } from 'antd'
+
+export default function Loading() {
+
+    let arrLoading = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    return (
+        <div className='loading container mx-auto'>
+            {
+                arrLoading.map((item, index) => {
+                    return (
+                        <div className='item' key={index}>
+                            <Skeleton.Image active />
+                            <Skeleton active paragraph={{ rows: 4 }} />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
--- src/pages/trade.tsx
+++ src/pages/trade.tsx
@@ -1,20 +1,21 @@
 import NormalLayout from 'components/layout/normalLayout'
 import { PageModel } from 'model/navModel'
 import { TradeItem } from 'model/trade'
-import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import Http from 'services/http'
-import { getAssetDetailPathByTradeItem } from 'utils/route'
-import { getAssetNameByType } from '../model/asset'
 import TradeComponent from '../components/trade/TradeComponent'
+import Loading from '../components/loading/loading'
 
 export default function TradePage() {
   let pageModel = new PageModel('Trade', 'WAVE', '')
   const [tradeItems, setTradeItems] = useState<Array<TradeItem>>()
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(() => {
     Http.getInstance()
       .getNFTTradeList(1, null)
       .then(response => {
+        setIsLoading(false)
         setTradeItems(response.data)
       })
       .catch(error => {
@@ -22,12 +23,20 @@ export default function TradePage() {
       })
   }, [])
 
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (!tradeItems) {
+    return <div className="empty">Empty Data</div>
+  }
+
   function content() {
     return (
       <div className="asset trade">
         <div className="container mx-auto">
           <h2>Trade</h2>
-          <ul>
+          <ul className='trade-item'>
             {tradeItems?.map((item, index) => {
               return <TradeComponent key={index} itemDate={item} />
             })}
