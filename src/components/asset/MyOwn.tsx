81a791d53cfe6ea670eb0f1023174c90f9d78e3c
liukeke <liukeke@diynova.com>
2022-11-04 20:48:06 +0800

    feat: assets page

---
+++ src/components/asset/MyOwn.tsx
@@ -0,0 +1,14 @@
+/*
+ * @Author: liukeke liukeke@diynova.com
+ * @Date: 2022-11-04 20:42:02
+ * @LastEditors: liukeke liukeke@diynova.com
+ * @LastEditTime: 2022-11-04 20:42:13
+ * @FilePath: /wave-app-webiste/src/components/asset/MyOwn.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+import React, { useEffect, useState } from 'react'
+
+export default function Myown(props) {
+  return <div className="evt-detail">my own</div>
+}
