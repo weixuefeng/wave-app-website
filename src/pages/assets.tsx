2ffe9ac69eaf36ee8be1fa653aa1cbce80fc8f7b
liukeke <liukeke@diynova.com>
2022-11-08 10:44:57 +0800

    feat: assets add header

--- src/pages/assets.tsx
+++ src/pages/assets.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-11-04 20:49:32
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-11-04 20:51:35
+ * @LastEditTime: 2022-11-07 13:50:26
  * @FilePath: /wave-app-webiste/src/pages/assets.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,7 +10,9 @@ import { Tab } from '@headlessui/react'
 import Mylistings from 'components/asset/MyListings'
 import Myoffers from 'components/asset/MyOffers'
 import Myown from 'components/asset/MyOwn'
+import NormalLayout from 'components/layout/normalLayout'
 import useWallet from 'hooks/userWallet'
+import { PageModel } from 'model/navModel'
 import { OfferType } from 'model/offer'
 import { UserInfo } from 'model/user'
 import React, { useEffect } from 'react'
@@ -19,6 +21,8 @@ import Http from 'services/http'
 import { useAppSelector } from 'store/store'
 
 export default function Assets() {
+
+  let pageModel = new PageModel('Trade', 'WAVE', '')
   const currentUser = useAppSelector(selectUser) as UserInfo
 
   useEffect(() => {
@@ -60,31 +64,35 @@ export default function Assets() {
       })
   }
 
-  return (
-    <div className="assets-page">
-      <div className="container mx-auto">
-        <h2 className="title">Assets</h2>
-        <div>
-          <Tab.Group>
-            <Tab.List>
-              <Tab className="tab-item">My Own</Tab>
-              <Tab className="tab-item mx-24">My Listings</Tab>
-              <Tab className="tab-item">My Offers</Tab>
-            </Tab.List>
-            <Tab.Panels>
-              <Tab.Panel>
-                <Myown />
-              </Tab.Panel>
-              <Tab.Panel>
-                <Mylistings />
-              </Tab.Panel>
-              <Tab.Panel>
-                <Myoffers />
-              </Tab.Panel>
-            </Tab.Panels>
-          </Tab.Group>
+  function content() {
+
+    return (
+      <div className="assets-page">
+        <div className="container mx-auto">
+          <h2 className="title">Assets</h2>
+          <div>
+            <Tab.Group>
+              <Tab.List>
+                <Tab className="tab-item">My Own</Tab>
+                <Tab className="tab-item mx-24">My Listings</Tab>
+                <Tab className="tab-item">My Offers</Tab>
+              </Tab.List>
+              <Tab.Panels>
+                <Tab.Panel>
+                  <Myown />
+                </Tab.Panel>
+                <Tab.Panel>
+                  <Mylistings />
+                </Tab.Panel>
+                <Tab.Panel>
+                  <Myoffers />
+                </Tab.Panel>
+              </Tab.Panels>
+            </Tab.Group>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+  return NormalLayout(content(), pageModel)
 }
