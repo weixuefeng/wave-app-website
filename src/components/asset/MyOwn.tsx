ce0dc5defb2a76200371166ad64e1530372fbe2a
liukeke <liukeke@diynova.com>
2022-11-10 20:07:00 +0800

    fix: current

--- src/components/asset/MyOwn.tsx
+++ src/components/asset/MyOwn.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-11-04 20:42:02
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-11-10 19:49:03
+ * @LastEditTime: 2022-11-10 20:05:26
  * @FilePath: /wave-app-webiste/src/components/asset/MyOwn.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -33,7 +33,7 @@ export default function Myown(props) {
   function getMyAssetList() {
     setIsLoading(true)
     Http.getInstance()
-      .getMyAssetList(currentPage, null)
+      .getMyAssetList(currentUser.id, currentPage)
       .then(response => {
         if (currentPage == 1) {
           // first page
@@ -64,15 +64,6 @@ export default function Myown(props) {
       .finally(() => {
         setIsLoading(false)
       })
-    // Http.getInstance()
-    //   .getMyAssetList(currentUser.id, 1)
-    //   .then(response => {
-    //     console.log('dddd',response)
-    //     setMyOwnData(response.data)
-    //   })
-    //   .catch(error => {
-    //     console.log(error)
-    //   })
   }
 
   useEffect(() => {
