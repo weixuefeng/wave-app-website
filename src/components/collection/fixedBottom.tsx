4daff1e26e732158952c09d579f901d4d16e13ff
weixuefeng <weixuefeng@diynova.com>
2022-11-14 14:18:04 +0800

    fix: buy number check

--- src/components/collection/fixedBottom.tsx
+++ src/components/collection/fixedBottom.tsx
@@ -2,7 +2,7 @@
  * @Author: zhuxiaotong zhuxiaotong@diynova.com
  * @Date: 2022-09-29 16:09:48
  * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-11-14 11:31:53
+ * @LastEditTime: 2022-11-14 14:17:46
  * @FilePath: /wave-app-website/src/components/collection/fixedBottom.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -95,8 +95,8 @@ function fixedBottom(props) {
         )
       case WhiteListSellStatus.TIME_SELLING:
         // check user had buy
-        const totalLimit = info.total_preemption_buy_number
-        const hasSaleWhiteList = info.white_list_settings.preemption_total_limit
+        const totalLimit = info.white_list_settings.preemption_total_limit
+        const hasSaleWhiteList = info.total_preemption_buy_number
         if (totalLimit > hasSaleWhiteList) {
           // continue buy
           // check user buy
