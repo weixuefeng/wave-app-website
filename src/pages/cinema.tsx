973d7d0650361d003f656e9444e786271aeaa8ea
liukeke <liukeke@diynova.com>
2022-11-16 13:52:17 +0800

    fix: cinema

--- src/pages/cinema.tsx
+++ src/pages/cinema.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-11-03 20:26:47
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-11-16 11:07:44
+ * @LastEditTime: 2022-11-16 13:51:41
  * @FilePath: /wave-app-website/src/pages/tickets.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -49,7 +49,6 @@ export default function Cinema(props) {
     Http.getInstance()
       .getMyCinemaList(currentPage)
       .then(response => {
-        console.log('ppppp', response)
         if (currentPage == 1) {
           // first page
           setCinemaData(response.data)
