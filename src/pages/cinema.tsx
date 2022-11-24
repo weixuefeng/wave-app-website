227633e9f0e9eee9b5627b56d552e7512ae429a5
liukeke <liukeke@diynova.com>
2022-11-24 15:22:02 +0800

    fix: css

--- src/pages/cinema.tsx
+++ src/pages/cinema.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-11-03 20:26:47
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-11-23 13:39:26
+ * @LastEditTime: 2022-11-24 15:14:11
  * @FilePath: /wave-app-webiste/src/pages/cinema.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -43,7 +43,7 @@ export default function Cinema(props) {
     return (
       <>
         <div className="cinema-page">
-          <div className="container mx-auto">
+          <div className="container mx-auto px-4 xl:px-0">
             <h2 className="title">
               <>{t('MY_CINEMA')}</>
             </h2>
