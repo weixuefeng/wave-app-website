1c54b6fa0230a252afc57f727b2ba7e1f119905d
weixuefeng <weixuefeng@diynova.com>
2022-11-29 15:38:19 +0800

    fix: add debug for api

--- src/pages/api/proxy.ts
+++ src/pages/api/proxy.ts
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-13 16:03:19
  * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-11-16 17:52:20
+ * @LastEditTime: 2022-11-29 15:38:05
  * @FilePath: /wave-app-website/src/pages/api/proxy.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,6 +23,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     http
       .post(url, params, config)
       .then(response => {
+        Log.d(`success data:${JSON.stringify(response.data)}`)
         res.status(200).json(response.data)
       })
       .catch(error => {
@@ -34,6 +35,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     http
       .get(url)
       .then(response => {
+        Log.d(`success data:${JSON.stringify(response.data)}`)
         res.status(200).json(response.data)
       })
       .catch(error => {
