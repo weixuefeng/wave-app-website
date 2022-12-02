43a0cb14c011aa747f01b088db6c99f2c550995f
liukeke <liukeke@diynova.com>
2022-12-02 20:32:19 +0800

    fix: img

--- src/components/asset/CollectionActivityComponent.tsx
+++ src/components/asset/CollectionActivityComponent.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-11-03 16:47:08
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-11-17 21:26:37
+ * @LastEditTime: 2022-12-02 20:28:15
  * @FilePath: /wave-app-webiste/src/components/asset/CollectionActivityComponent.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -26,29 +26,29 @@ export default function CollectionActivity(props) {
   return (
     <>
       <div className="collection-activity">
-        {data?.map((item, index) => {
+        {data && data?.map((item, index) => {
           return (
             <div className="item" key={index}>
               <div className="event">{item.event}</div>
               <div className="info">
                 <div>
-                  <img src={item.from_user.avatar} alt={item.nft.name} />
+                  <img src={item.nft.image} alt={item.nft.name} />
                 </div>
                 <h3>{item.nft.name}</h3>
               </div>
               <div className="price">{floorNum(item.price)} NEW</div>
               <div className="trading">
                 <div className="form">
-                  <img src={item.from_user.avatar} alt="avatar" />
+                  <img src={item.from_user.avatar == '' ? '/assets/image/icon_avata.png' : item.from_user.avatar} alt="avatar" />
                 </div>
                 <h4>{trimStr(item.from_user.name)}</h4>
                 <div className="arrow">
                   <img src="/assets/image/icon_arrow.png" alt="arrow-img" />
                 </div>
                 <div className="to">
-                  <img src={item.to_user.avatar} alt="avatar" />
+                  <img src={item.to_user.avatar == '' ? '/assets/image/icon_avata.png' : item.to_user.avatar} alt="avatar" />
                 </div>
-                <h4>{item.to_user.name}</h4>
+                <h4>{trimStr(item.to_user.name)}</h4>
               </div>
               <div className="date">{formatDate(item.activity_time)}</div>
             </div>
