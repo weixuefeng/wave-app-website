e19df5c7d293d8f48853791a7ca0e74bb1938a72
weixuefeng <weixuefeng@diynova.com>
2022-12-08 21:36:59 +0800

    fix: build

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -1,13 +1,12 @@
-import { enc } from 'crypto-js';
-import AES from 'crypto-js/aes';
-import Log from './log';
+// import { enc } from 'crypto-js';
+// import AES from 'crypto-js/aes';
 
-export function aesEncrypt(key: string, data: string): string {
-  const ciphertext = AES.encrypt(JSON.stringify({ data }), key);
-  const res = ciphertext.toString();
-  return res;
-}
+// export function aesEncrypt(key: string, data: string): string {
+//   const ciphertext = AES.encrypt(JSON.stringify({ data }), key);
+//   const res = ciphertext.toString();
+//   return res;
+// }
 
-export function aesDecrypt(key: string, data: string): string {
-  return AES.decrypt(enc.Base64.parse(data).toString(enc.Utf8), key).toString(enc.Utf8);
-}
+// export function aesDecrypt(key: string, data: string): string {
+//   return AES.decrypt(enc.Base64.parse(data).toString(enc.Utf8), key).toString(enc.Utf8);
+// }
